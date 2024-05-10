c807f52a206eb7de68f53ed1d3715b0982da7130
lashagamgoneishvili <gamgoneishvili8@gamil.com>
2024-05-10 15:12:43 +0400

    created new 'users' table

--- app/[locale]/(dashboard)/admin/page.tsx
+++ app/[locale]/(dashboard)/admin/page.tsx
@@ -5,7 +5,6 @@ import Button from "../../../../components/Button";
 
 export default async function UsersPage() {
   const users = await getUsers();
-
   return (
     <div className="px-5 bg-slate-400 h-screen w-full flex justify-center  flex-col">
       <form
@@ -14,6 +13,7 @@ export default async function UsersPage() {
       >
         <input type="text" name="name" required />
         <input type="text" name="email" required />
+        <input type="number" name="age" required />
         <button type="submit" className="bg-red-300 text-white">
           Create User
         </button>
@@ -24,6 +24,7 @@ export default async function UsersPage() {
           <div className="flex gap-4 border-b">
             <h1>{user.name}</h1>
             <p>{user.email}</p>
+            <p>{user.age}</p>
           </div>
 
           <Button id={user.id} />
--- app/api/create-users-table/route.ts
+++ app/api/create-users-table/route.ts
@@ -4,9 +4,19 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const result =
-      await sql`CREATE TABLE users (id SERIAL, name varchar(255), email varchar(255) );`;
+      await sql`CREATE TABLE users (id SERIAL, name varchar(255), email varchar(255), age INT );`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
 }
+
+// Remove table from DB
+// export async function POST() {
+//   try {
+//     const result = await sql`DROP TABLE IF EXISTS users;`;
+//     return NextResponse.json({ result }, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json({ error }, { status: 500 });
+//   }
+// }
