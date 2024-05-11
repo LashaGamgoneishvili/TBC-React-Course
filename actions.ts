390289a82410b7110bef4fd218098bc53dd81e29
lashagamgoneishvili <gamgoneishvili8@gamil.com>
2024-05-11 15:46:23 +0400

    add edit functionality

--- actions.ts
+++ actions.ts
@@ -1,5 +1,5 @@
 "use server";
-import { createUser, deleteUser } from "./api";
+import { createUser, deleteUser, updateUser } from "./api";
 
 export async function createUserAction(formData: FormData) {
   const name = formData.get("name");
@@ -7,6 +7,15 @@ export async function createUserAction(formData: FormData) {
   const age = formData.get("age");
 
   createUser(name as string, email as string, age as string);
+}
+
+export async function updateUserAction(formData: FormData) {
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const age = formData.get("age");
+  const id = formData.get("id");
+
+  updateUser(name as string, email as string, age as string, id as string);
 }
 
 export async function deleteUserAction(id: number) {
