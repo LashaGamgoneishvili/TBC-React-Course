b19ff1572048d6dd40604e966ee589ac92b4ef71
LashaGamgoneishvili <133699668+LashaGamgoneishvili@users.noreply.github.com>
2024-06-13 01:58:48 +0400

    fix login-logout bug

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -2,7 +2,7 @@ import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
 export const GET = handleAuth({
   login: handleLogin({
-    returnTo: "/api/user-api/create-user",
+    returnTo: "/api/user-api/create-user-login",
     authorizationParams: {
       prompt: "login",
     },
